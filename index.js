88ffe86dca25b07fc078afe72b3680ae32df28bf
hishamss <hishamss90@gmail.com>
2020-04-10 20:05:11 -0700

    added user validation in index.js

--- index.js
+++ index.js
@@ -2,5 +2,31 @@ var Word = require("./Word.js");
 var inquirer = require("inquirer");
 var words = ["cat", "dog", "lion", "tiger", "zebra"];
 
-var wordtoguess = words[Math.floor(Math.random() * words.length)];
-console.log(wordtoguess);
+function generate() {
+  var wordtoguess = words[Math.floor(Math.random() * words.length)];
+  console.log(wordtoguess);
+  var wordtoguessOjb = new Word.word(wordtoguess);
+}
+
+function playgame() {
+  inquirer
+    .prompt([
+      {
+        name: "letter",
+        message: "Guess a letter :",
+        validate: function (value) {
+          // accept only one character
+          if (value.length == 1) {
+            return true;
+          }
+          return false;
+        },
+      },
+    ])
+    .then(function (answer) {
+      console.log(answer.letter);
+      playgame();
+    });
+}
+
+playgame();
